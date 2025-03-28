699cba99a8e96e9f8003dd82b69b62cb104aa00e
Jakub Kuniszewski <you@example.com>
2025-03-28 19:33:20 +0100

    Add tool

---
+++ Dotnet.Markdownify.Tool/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Dotnet.Markdownify;
+
+var inputHtmlPath = args[0];
+var outputMdPath = args[1];
+
+try
+{
+    var converter = new MarkdownConverter();
+    var html = await File.ReadAllTextAsync(inputHtmlPath);
+    var md = await converter.ConvertAsync(html);
+
+    await File.WriteAllTextAsync(outputMdPath, md);
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Error: {ex.Message}");
+}
